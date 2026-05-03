0012122dedfb6566f8b7fdf9a8f75f77247b0e64
Victor Edwin Reyes <victorereyesgg@gmail.com>
2026-05-03 13:52:14 +0800

    feat(db/schema): add debt table

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,24 @@
-import { pgTable, serial, integer, text } from 'drizzle-orm/pg-core';
+import { pgTable, text, uuid, varchar, decimal, pgEnum, timestamp } from 'drizzle-orm/pg-core';
+import { user } from './auth.schema';
 
-export const task = pgTable('task', {
-	id: serial('id').primaryKey(),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
-});
+const debtStatus = pgEnum('debt_status', [
+	'pending_adoption',
+	'unpaid',
+	'payment_pending',
+	'settled'
+])
+
+export const debt = pgTable('debt', {
+	id: uuid('id').defaultRandom().primaryKey(),
+	shareToken: varchar('share_token'),
+	lenderId: text('lender_id').references(() => user.id),
+	borrowerName: varchar('borrower_name'),
+	borrowerId: text('borrower_id').references(() => user.id),
+	amountOwned: decimal('amount_owned', { precision: 10, scale: 2 }),
+	description: text('description'),
+	status: debtStatus().notNull(),
+	createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+	updatedAt: timestamp('updated_at', { withTimezone: true }).defaultNow(),
+})
 
 export * from './auth.schema';

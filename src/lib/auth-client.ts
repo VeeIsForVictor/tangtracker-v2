6b287cadf8b50c40fb148854dcaf215e0e8f4025
Victor Edwin Reyes <victorereyesgg@gmail.com>
2026-05-03 18:56:12 +0800

    feat: add typing for auth data

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,3 +3,6 @@ import { createAuthClient } from "better-auth/svelte"
 export const authClient = createAuthClient({
     baseURL: "http://localhost:5173"
 })
+
+export type Session = typeof authClient.$Infer.Session
+export type User = typeof authClient.$Infer.Session.user

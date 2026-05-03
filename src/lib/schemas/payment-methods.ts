4ef30523380ffd6f99741228a2877058faf6af80
Victor Edwin Reyes <victorereyesgg@gmail.com>
2026-05-04 06:15:36 +0800

    feat: add payment_methods json field for user schema

---
+++ src/lib/schemas/payment-methods.ts
@@ -0,0 +1,13 @@
+import { array, object, picklist, string, type InferOutput } from "valibot";
+
+export const PaymentProvider = picklist(['Maya', 'GCash']);
+
+export const PaymentMethod = object({
+    provider: PaymentProvider,
+    contact: string()
+});
+
+export const PaymentMethods = array(PaymentMethod);
+
+export type PaymentMethod = InferOutput<typeof PaymentMethod>;
+export type PaymentMethods = InferOutput<typeof PaymentMethods>;
--- src/lib/server/db/auth.schema.ts
+++ src/lib/server/db/auth.schema.ts
@@ -1,5 +1,6 @@
+import type { PaymentMethods } from '$lib/schemas/payment-methods';
 import { relations } from 'drizzle-orm';
-import { pgTable, text, timestamp, boolean, index } from 'drizzle-orm/pg-core';
+import { pgTable, text, timestamp, boolean, index, json } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
 	id: text('id').primaryKey(),
@@ -7,6 +8,7 @@ export const user = pgTable('user', {
 	email: text('email').notNull().unique(),
 	emailVerified: boolean('email_verified').default(false).notNull(),
 	image: text('image'),
+	paymentMethods: json('payment_methods').$type<PaymentMethods>(),
 	createdAt: timestamp('created_at').defaultNow().notNull(),
 	updatedAt: timestamp('updated_at')
 		.defaultNow()

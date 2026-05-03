b7a5da023c73fe8ee8d9487ce549f6100132ff22
Victor Edwin Reyes <victorereyesgg@gmail.com>
2026-05-03 19:10:30 +0800

    feat: setup profile page

---
+++ src/routes/(auth)/profile/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    const { data } = $props();
+
+    let { user } = $derived(data);
+</script>
+
+<div class="flex flex-col items-center bg-slate-600/30 p-8 rounded-2xl">
+    <h1 class="text-xl">{user.name}</h1>
+    <h3 class="text-md">{user.email}</h3>
+</div>

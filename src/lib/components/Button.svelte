a89df8f0f8e866b62a10567819a5b8ae605ed424
Victor Edwin Reyes <victorereyesgg@gmail.com>
2026-05-03 15:12:53 +0800

    feat: add Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import type { ResolvedPathname } from '$app/types';
+	import { cn } from '$lib/utils';
+	import type { Snippet } from 'svelte';
+	import type { ClassValue } from 'svelte/elements';
+
+    interface Props {
+        display?: 'filled' | 'outline'
+        className?: ClassValue
+        type?: 'button' | 'submit'
+        href?: ResolvedPathname
+        children: Snippet
+    }
+
+    const { href = undefined, className = "", type = 'button', display, children }: Props = $props();
+
+    let setClass: ClassValue = $state("");
+
+    $effect(() => {
+        switch (display) {
+            case 'filled': 
+                setClass = "p-2 w-full rounded-lg bg-white text-black"
+                break
+            case 'outline':
+                setClass = "p-2 w-full rounded-lg bg-none border-gray-200 border-2"
+                break
+        }   
+    })
+</script>
+
+{#if (typeof href !== 'undefined')}
+    <a {href} class={cn(className, setClass)}>
+        {@render children()}
+    </a>
+{:else}
+    <button {type} class={cn(className, setClass)}>
+        {@render children()}
+    </button>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,15 @@
-<div class="flex flex-col items-center text-center gap-y-4">
+<script>
+	import Button from "$lib/components/Button.svelte";
+
+</script>
+<div class="flex flex-col items-center text-center gap-y-2">
     <h1 class="text-3xl">Welcome to <span class="underline">TangTracker</span></h1>
     <p>Utang, tracked!</p>
+    <div class="flex flex-col items-center mt-8 gap-y-4">
+        <Button display="filled" href="/login">
+            Login
+        </Button>
+        <button class="p-2 w-full rounded-lg bg-white text-black" type="button">Login</button>
+        <button class="p-2 w-full rounded-lg bg-none border-gray-200 border-2" type="button">Register</button>
+    </div>
 </div>

0a3309d5b6447e181e48261f78bf2c015077db02
Victor Edwin Reyes <victorereyesgg@gmail.com>
2026-05-04 07:14:09 +0800

    feat: add user-aware layout features

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
 	import './layout.css';
+	import "iconify-icon"
 	import favicon from '$lib/assets/favicon.svg';
 	import Background from '$lib/components/Background.svelte';
+	import Button from '$lib/components/Button.svelte';
 
-	let { children } = $props();
+	let { data, children } = $props();
+
+	const { isLoggedIn } = $derived(data);
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <Background />
+
+<div class="absolute top-0">
+	{#if isLoggedIn}
+		<Button display="filled" href="/profile" className="m-4 p-2 size-auto">
+			<iconify-icon icon="material-symbols:location-home" width="24" height="24"></iconify-icon>
+		</Button>
+	{/if}
+</div>
 
 <div class="m-auto flex h-dvh flex-col items-center justify-center text-white">
 	{@render children()}

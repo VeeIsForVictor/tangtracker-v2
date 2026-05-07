086c33e639b870dcd93e47b32a9094868b1c53c1
Victor Edwin Reyes <victorereyesgg@gmail.com>
2026-05-08 06:55:57 +0800

    feat: complete new tab form ui

---
+++ src/routes/(auth)/new/+page.server.ts
@@ -0,0 +1,9 @@
+import { strict } from "assert/strict";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ locals }) => {
+    strict(typeof locals.user !== 'undefined');
+    return {
+        user: locals.user
+    };
+};

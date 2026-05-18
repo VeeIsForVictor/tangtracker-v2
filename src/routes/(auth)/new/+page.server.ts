86a87baa2cc94d470dd37dd0968b8fff3724018a
Victor Edwin Reyes <victorereyesgg@gmail.com>
2026-05-18 21:37:16 +0800

    chore: run formatter

--- src/routes/(auth)/new/+page.server.ts
+++ src/routes/(auth)/new/+page.server.ts
@@ -1,38 +1,37 @@
-import * as v from 'valibot'
-import { strict } from "assert/strict";
-import type { Actions, PageServerLoad } from "./$types";
+import * as v from 'valibot';
+import { strict } from 'assert/strict';
+import type { Actions, PageServerLoad } from './$types';
 import { error, redirect } from '@sveltejs/kit';
 import { resolve } from '$app/paths';
 
 export const load: PageServerLoad = async ({ locals }) => {
-    strict(typeof locals.user !== 'undefined');
-    return {
-        user: locals.user
-    };
+	strict(typeof locals.user !== 'undefined');
+	return {
+		user: locals.user
+	};
 };
 
 export const actions: Actions = {
-    default: async ({ fetch, request, locals }) => {
-        const data = await request.formData();
-        const lenderId = data.get('lenderId');
-        const borrower = data.get('borrower');
-        const amountOwed = data.get('amountOwed');
-        const description = data.get('description');
-        
-        const user = locals.user;
-        if (typeof user === 'undefined')
-            error(401, 'Not Authorized');
-        const response = await fetch('/api/v1/tabs', {
-            method: 'POST',
-            body: JSON.stringify({
-                lenderId,
-                borrowerName: borrower,
-                amountOwed,
-                description
-            })
-        })
+	default: async ({ fetch, request, locals }) => {
+		const data = await request.formData();
+		const lenderId = data.get('lenderId');
+		const borrower = data.get('borrower');
+		const amountOwed = data.get('amountOwed');
+		const description = data.get('description');
 
-        const { newId } = v.parse(v.object({ newId: v.string() }), await response.json());
-        redirect(303, resolve('/t/[id]', { id: newId }))
-    }
+		const user = locals.user;
+		if (typeof user === 'undefined') error(401, 'Not Authorized');
+		const response = await fetch('/api/v1/tabs', {
+			method: 'POST',
+			body: JSON.stringify({
+				lenderId,
+				borrowerName: borrower,
+				amountOwed,
+				description
+			})
+		});
+
+		const { newId } = v.parse(v.object({ newId: v.string() }), await response.json());
+		redirect(303, resolve('/t/[id]', { id: newId }));
+	}
 };
--- src/routes/(auth)/new/+page.svelte
+++ src/routes/(auth)/new/+page.svelte
@@ -1,73 +1,54 @@
 <script lang="ts">
-	import { enhance } from "$app/forms";
-	import Button from "$lib/components/Button.svelte";
-	import { createForm } from "@tanstack/svelte-form";
+	import { enhance } from '$app/forms';
+	import Button from '$lib/components/Button.svelte';
+	import { createForm } from '@tanstack/svelte-form';
 
-    const { data } = $props();
-    const { user: { id: lenderId } } = $derived(data);
+	const { data } = $props();
+	const {
+		user: { id: lenderId }
+	} = $derived(data);
 
-    const form = createForm(() => ({
-        defaultValues: {
-            lenderId,
-            borrower: '',
-            amountOwed: '',
-            description: ''
-        }
-    }))
+	const form = createForm(() => ({
+		defaultValues: {
+			lenderId,
+			borrower: '',
+			amountOwed: '',
+			description: ''
+		}
+	}));
 </script>
 
 <div class="flex flex-col space-y-8">
-    <div class="flex flex-col items-center">
-        <h1 class="text-3xl">New Tab</h1>
-        <h5 class="text-sm">Track a new accountability</h5>
-    </div>
-    
-    <form
-        class="flex flex-col gap-y-4 rounded-xl bg-slate-900/60 p-4"
-        method="POST"
-        use:enhance
-    >
-        <form.Field name="borrower">
-            {#snippet children(field)}
-                <div class="flex flex-row items-center space-x-2">
-                    <label for={field.name}>
-                        Borrower:
-                    </label>
-                    <input
-                        name={field.name}
-                        class="w-full rounded-lg bg-gray-800/20 p-1"
-                    />
-                </div>
-            {/snippet}
-        </form.Field>
-        <form.Field name="amountOwed">
-            {#snippet children(field)}
-                <div class="flex flex-row items-center space-x-2">
-                    <label for={field.name}>
-                        Amount:
-                    </label>
-                    <input
-                        name={field.name}
-                        class="w-full rounded-lg bg-gray-800/20 p-1"
-                    />
-                </div>
-            {/snippet}
-        </form.Field>
-        <form.Field name="description">
-            {#snippet children(field)}
-                <div class="flex flex-col space-y-2">
-                    <label for={field.name}>
-                        Description:
-                    </label>
-                    <textarea
-                        name={field.name}
-                        class="w-full rounded-lg bg-gray-800/20 p-1"
-                    ></textarea>
-                </div>
-            {/snippet}
-        </form.Field>
-        <Button type="submit" display="filled">
-            Create
-        </Button>
-    </form>
+	<div class="flex flex-col items-center">
+		<h1 class="text-3xl">New Tab</h1>
+		<h5 class="text-sm">Track a new accountability</h5>
+	</div>
+
+	<form class="flex flex-col gap-y-4 rounded-xl bg-slate-900/60 p-4" method="POST" use:enhance>
+		<form.Field name="borrower">
+			{#snippet children(field)}
+				<div class="flex flex-row items-center space-x-2">
+					<label for={field.name}> Borrower: </label>
+					<input name={field.name} class="w-full rounded-lg bg-gray-800/20 p-1" />
+				</div>
+			{/snippet}
+		</form.Field>
+		<form.Field name="amountOwed">
+			{#snippet children(field)}
+				<div class="flex flex-row items-center space-x-2">
+					<label for={field.name}> Amount: </label>
+					<input name={field.name} class="w-full rounded-lg bg-gray-800/20 p-1" />
+				</div>
+			{/snippet}
+		</form.Field>
+		<form.Field name="description">
+			{#snippet children(field)}
+				<div class="flex flex-col space-y-2">
+					<label for={field.name}> Description: </label>
+					<textarea name={field.name} class="w-full rounded-lg bg-gray-800/20 p-1"></textarea>
+				</div>
+			{/snippet}
+		</form.Field>
+		<Button type="submit" display="filled">Create</Button>
+	</form>
 </div>
